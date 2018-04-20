521d27772eba927725b7775752048926aab6c41d
Michael Boroski <mjboroski@gmail.com>
2018-04-20 22:56:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -14,7 +14,10 @@ def sort_array_char_count (array)
 end
 
 def swap_elements (array)
-
+  holder=array[1]
+  array[1]=array[2]
+  array[2]=holder
+  return array
 end
 
 def swap_elements_from_to (array, index, destination_index)
