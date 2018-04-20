2c01e4b42a3b5d0d061e3479b309c9a7929e3166
Michael Boroski <mjboroski@gmail.com>
2018-04-20 22:49:22 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
+def sort_array_asc (array)
 
+end
+
+def sort_array_desc (array)
+
+end
+
+def sort_array_char_count (array)
+
+end
+
+def swap_elements (array)
+
+end
+
+def swap_elements_from_to (array, index, destination_index)
+
+end
+
+def reverse_array (array)
+
+end
+
+def kesha_maker (array)
+
+end
+
+def find_a (array)
+
+end
+
+def sum_array (array)
+
+end
+
+def add_s (array)
+
+end
